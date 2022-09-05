5bc9742f944af414c879c0fe444f5d08906ee8cb
RS-gty <18920702372@163.com>
2022-09-05 20:11:19 +0800

    Change

--- QuantumChemistry.py
+++ QuantumChemistry.py
@@ -3,6 +3,7 @@
 import random as r
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 
 # Calculating
@@ -70,7 +71,33 @@ def SphericalTo3DAxis(array: np.ndarray):
     return np.array([x, y, z])
 
 
+def SphericalTo3DAxis_Matrix(array: list):
+    """
+    change coordinate in a Spherical coordinate system into 3D-axis
+    :param array: in the order of radius, theta and phi
+    :return: an array that contains x, y, z coordinate in a 3D-axis
+    """
+    radius = array[0]
+    theta = array[1]
+    phi = array[2]
+    rad = []
+    the = []
+    ph = []
+    for i in range(len(radius)):
+        rad.append(float(radius[i]) * np.sin(theta[i]) * np.cos(phi[i]))
+        the.append(float(radius[i]) * np.sin(theta[i]) * np.sin(phi[i]))
+        ph.append(float(radius[i]) * np.cos(theta[i]))
+    return [np.array(rad), np.array(the), np.array(ph)]
+
+
 # Wave Functions
+def RadialWaveFunction_1s(radius):
+    return 2 * np.sqrt(1 / power(1, 0.529, 3)) * np.exp(-radius / 0.529)
+
+
+def AngleWaveFunction_1s(theta, phi):
+    return np.sqrt(1 / 4 * np.pi)
+
 
 if __name__ == '__main__':
     """
@@ -87,6 +114,20 @@ if __name__ == '__main__':
 
     plt.scatter(q * np.sin(t), q * np.cos(t), s=0.00001)
     plt.show()
-    """
 
-    print(SphericalTo3DAxis(np.array([np.sqrt(3), np.pi / 2 - np.arctan(np.sqrt(2)/2), np.pi / 4])))
+    print(SphericalTo3DAxis(np.array([np.sqrt(3), np.pi / 2 - np.arctan(np.sqrt(2) / 2), np.pi / 4])))
+    """
+    a = np.arange(0, 4, 0.00005)
+    b = RadialWaveFunction_1s(a)
+    c = 200000
+    r_sampled = 2.5 * rand(1, c)
+    theta = rand(1, c) * 2 * np.pi
+    phi = rand(1, c) * 2 * np.pi
+
+    F = SphericalTo3DAxis_Matrix([r_sampled[0].tolist(), theta.tolist()[0], phi.tolist()[0]])
+    f = np.array(F)
+
+    ax = plt.axes(projection='3d')
+    ax.scatter3D(f[0], f[1], f[2], 'gray', s=0.00001)
+    ax.set_title('3D line plot')
+    plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,34 @@
 import numpy as np
 from numpy import linalg
+from scipy.linalg import sqrtm
+
+a = np.matrix([[1.0, 4.0, 6.0], [4.0, 20.0, 34.0], [6.0, 34.0, 70.0]])
+b = np.matrix([[1, 0, 0, 0], [5, 2, 0, 0], [8, 6, 3, 0], [10, 9, 7, 4]])
+
+
+def GetRange(i: int, j: int) -> list:
+    ran = []
+    for n in range(i, j+1):
+        ran.append(n)
+    if i > j:
+        return [None]
+    elif i == j:
+        return [i]
+    else:
+        return ran
+
+
+def IsSymmetryPositiveDefiniteMatrix(matrix: np.ndarray):
+    if np.array_equal(matrix, matrix.T):
+        try:
+            np.linalg.cholesky(matrix)
+            return True
+        except np.linalg.LinAlgError:
+            return False
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(b*b.T)
 
