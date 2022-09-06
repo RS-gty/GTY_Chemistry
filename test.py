b5aca15605a48f769ab26d872633a5061087099c
RS-gty <18920702372@163.com>
2022-09-06 16:27:42 +0800

    Change

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+import base64
+
 import numpy as np
 from numpy import linalg
 from scipy.linalg import sqrtm
+from CHEM import *
 
 a = np.matrix([[1.0, 4.0, 6.0], [4.0, 20.0, 34.0], [6.0, 34.0, 70.0]])
 b = np.matrix([[1, 0, 0, 0], [5, 2, 0, 0], [8, 6, 3, 0], [10, 9, 7, 4]])
@@ -29,6 +32,12 @@ def IsSymmetryPositiveDefiniteMatrix(matrix: np.ndarray):
         return False
 
 
+def Cal1(l1):
+    return [abs(l1[0]-l1[1]), abs(l1[1]-l1[2]), abs(l1[2]-l1[3]), abs(l1[3]-l1[0])]
+
+
 if __name__ == '__main__':
-    print(b*b.T)
+    b = bytes('abcdefgh'.encode('utf-8'))
+    print(bytes('abcdefgh'.encode('utf-8')))
+    print(str(b))
 
